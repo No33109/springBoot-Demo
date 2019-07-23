8abbadc103ce7ba61197d4aa4be13b39b741b575
No33109 <15818470854@163.com>
2019-07-23 13:58:35 +0800

    spring boot kafka test get message

--- src/main/java/com/example/demo/v1/controller/kafka/KafkaController.java
+++ src/main/java/com/example/demo/v1/controller/kafka/KafkaController.java
@@ -2,12 +2,18 @@ package com.example.demo.v1.controller.kafka;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 @RestController
 @RequestMapping("/kafka")
@@ -17,6 +23,10 @@ public class KafkaController {
     @Resource
     private KafkaTemplate<String,String> kafkaTemplate;
 
+    /**
+     * 生产信息
+     * @return
+     */
     @RequestMapping("/test")
     public String testKafka() {
         int iMax = 100;
@@ -29,5 +39,33 @@ public class KafkaController {
     @KafkaListener(topics = "test")
     public void receive(ConsumerRecord<?, ?> consumer) {
         log.info("{} - {}:{}", consumer.topic(), consumer.key(), consumer.value());
+    }
+
+
+    /**
+     * 消费信息
+     */
+    @RequestMapping("/get")
+    public Map<String, String> testKafkaGet(){
+        Map<String,String> map = new HashMap<>(16);
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "kafka2");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String,String> consumer = new KafkaConsumer<>(props);
+       //订阅主题列表topic
+        consumer.subscribe(Arrays.asList("test","__consumer_offsets"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+//                System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value()+"\n");
+                map.put(record.key(), record.value());
+            }
+        }
     }
 }
