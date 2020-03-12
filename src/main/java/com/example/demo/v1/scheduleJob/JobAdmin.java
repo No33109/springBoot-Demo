5363dfb56f93915358e00cf869c156de22c421b3
lrz <15818470854@163.com>
2020-03-12 09:15:05 +0800

    logger fix

--- src/main/java/com/example/demo/v1/scheduleJob/JobAdmin.java
+++ src/main/java/com/example/demo/v1/scheduleJob/JobAdmin.java
@@ -1,5 +1,7 @@
 package com.example.demo.v1.scheduleJob;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -7,15 +9,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class JobAdmin {
 
+    private static Logger logger = LoggerFactory.getLogger(JobAdmin.class);
+
     @Autowired
    private  JobTimeConfig jobTimeConfig;
 
     @Scheduled(cron = "${admin.job.corn}")
     public void handleAllJob(){
         try {
+            logger.warn("JobAdmin handleAllJob start");
             jobTimeConfig.findAllClassByInterface(BaseJob.class);
         }catch (Exception e){
-            e.printStackTrace();
+            logger.error("JobAdmin handleAllJob error",e);
         }
     }
 }
--- src/main/java/com/example/demo/v1/scheduleJob/TestJob.java
+++ src/main/java/com/example/demo/v1/scheduleJob/TestJob.java
@@ -1,11 +1,15 @@
 package com.example.demo.v1.scheduleJob;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TestJob implements BaseJob {
+
+    private static Logger logger = LoggerFactory.getLogger(TestJob.class);
 
     @Autowired
     private JobTimeConfig jobTimeConfig;
@@ -14,7 +18,7 @@ public class TestJob implements BaseJob {
     @Override
     @Scheduled(cron = "${com.example.demo.v1.scheduleJob.TestJob}")
     public void process() {
-        System.out.println("定时----");
-        jobTimeConfig.findAllClassByInterface(BaseJob.class);
+//        System.out.println("定时----");
+//        jobTimeConfig.findAllClassByInterface(BaseJob.class);
     }
 }
